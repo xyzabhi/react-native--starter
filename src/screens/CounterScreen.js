1672ae21a30ccc0386b245afce96c872c36b6e37
Abhinav Kumar <abhinavkumar8083@gmail.com>
2022-05-31 04:11:45 +0530

    Counter Screen Added that have counter state.useState() Hook used .

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,17 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
 
 const CounterScreen = () => {
+  const [counter, setCounter] = useState(0);
   return (
     <View>
       <Button
         title="Increase"
         onPress={() => {
-          counter++;
+          setCounter(counter + 1);
         }}
       />
-      <Button title="Decrease" onPress={() => counter--} />
-      <Text>Counter is {counter}</Text>
+      <Button title="Decrease" onPress={() => setCounter(counter - 1)} />
+      <Text>Counter {counter}</Text>
     </View>
   );
 };
